dcebe59908c887cde28fd07d16d7a54289c82616
Storgisl <antipov03306@list.ru>
2024-04-17 22:59:31 +0300

    commit

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,25 +1,49 @@
+from logging import PlaceHolder
+from xmlrpc.client import Boolean
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class UserRegisterForm(forms.Form):
+    username = forms.CharField(
+                        max_length=30, 
+                        required=True,
+                        widget= forms.TextInput
+                           (attrs={
+                               'placeholder':'email or username',
+                               }))
+    
+    password1 = forms.CharField(
+                            widget=forms.PasswordInput(), 
+                            required=True)
+    
+    password2 = forms.CharField(
+                            widget=forms.PasswordInput(), 
+                            required=True)
+    
+    birthday = forms.DateField(label="Date of Birth",
+                                required=True,
+                                widget=forms.DateInput(
+                                format="%Y-%m-%d", attrs={"type": "date"}),
+                                input_formats=["%Y-%m-%d"])
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-    
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password1'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
-    
 class UserLoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=30, widget= forms.TextInput(
+                            attrs={
+                                'placeholder':'email or username',
+                                }),
+                           required=True)
+    
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':'password',
+            }
+        ),
+        required=True,
+    )
 
 # Create a UserUpdateForm to update a username and email
 class UserUpdateForm(forms.ModelForm):

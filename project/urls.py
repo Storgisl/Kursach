5ac9522c2d92a4391c7e92cc99b5769836338c66
Storgisl <antipov03306@list.ru>
2024-04-25 01:22:17 +0300

    commit

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +30,11 @@ urlpatterns = [
     path('', include('videos.urls'), name='videos'),
     path('', include('reg_page.urls'), name='registration'),
     path('', include('log_page.urls'), name='login'),
+        path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
